ac954990a46982d0126d7719966147cf84fb41ef
nguyenthaiduong1611 <nguyenthaiduong1611@gmail.com>
2022-06-19 14:59:14 +0700

    update danh sach lich hen

--- src/main/java/com/example/doanweb/service/mapper/ServiceMapper2.java
+++ src/main/java/com/example/doanweb/service/mapper/ServiceMapper2.java
@@ -1,10 +1,10 @@
-package com.example.doanweb.service.mapper;
-
-import org.mapstruct.Mapper;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-import com.example.doanweb.entity.Services;
-import com.example.doanweb.service.dto.ServiceDTO;
-
-@Mapper(componentModel = "spring",uses = {})
-public interface ServiceMapper2 extends EntityMapper<ServiceDTO, Services>{}
+//package com.example.doanweb.service.mapper;
+//
+//import org.mapstruct.Mapper;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.stereotype.Component;
+//import com.example.doanweb.entity.Services;
+//import com.example.doanweb.service.dto.ServiceDTO;
+//
+//@Mapper(componentModel = "spring",uses = {})
+//public interface ServiceMapper2 extends EntityMapper<ServiceDTO, Services>{}
